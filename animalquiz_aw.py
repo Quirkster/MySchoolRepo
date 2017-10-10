0e9dbecfa1f7f3c6a1c5a35a05b3e46bda88ab49
Quirkster <anna@weiksner.com>
2017-10-10 07:52:51 -0400

    First Commit - MySchoolRepo

---
+++ animalquiz_aw.py
@@ -0,0 +1,8 @@
+x = input("what's your favorite animal")
+if x == 'cat':
+    print("whoa mine too")
+elif x == 'dog':
+    print("that's my favorite fave!")
+elif x == 'parrot':
+    print("cool")
+
