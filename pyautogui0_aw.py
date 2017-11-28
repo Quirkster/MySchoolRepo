2b28d89c8841483fec292c23cda3889f23181631
Quirkster <anna@weiksner.com>
2017-11-28 08:00:46 -0500

    fixed opening tab

--- pyautogui0_aw.py
+++ pyautogui0_aw.py
@@ -1,13 +1,15 @@
 import pyautogui
 def main():
-    pyautogui.moveTo(100,27,duration=2)
-    pyautogui.click(x=100, y=27, clicks=1, interval=1, button='left')
+    #pyautogui.moveTo(100,27,duration=1)
+    pyautogui.click(x=24, y=738, clicks=1, interval=1, button='left')
+    pyautogui.typewrite('chrome\n', interval=0.1)
+    pyautogui.click(x=100, y=27, clicks=1, interval=0.5, button='left')
     pyautogui.scroll(600,x=682,y=155)
-    pyautogui.click(x=541, y=35, clicks=1, interval=1, button='left')
-    pyautogui.typewrite('anna is awesome\n', interval=1)
+    pyautogui.hotkey('ctrl','t')
+    pyautogui.typewrite('anna is awesome\n', interval=0.5)
     pyautogui.typewrite(' \n', interval=3)
-    pyautogui.click(x=479, y=28, clicks=1, interval=1, button='left')
-    pyautogui.click(x=438,y=42,clicks=1,interval=1,button='left')
-    pyautogui.click(x=316,y=39, clicks=1, interval=1, button='left')
+    pyautogui.click(x=479, y=28, clicks=1, interval=0.5, button='left')
+    pyautogui.click(x=438,y=42,clicks=1,interval=0.5,button='left')
+    pyautogui.click(x=316,y=39, clicks=2, interval=0.5, button='left')
 if __name__ == '__main__':
     main()
